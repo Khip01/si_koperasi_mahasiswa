d0d36751e6d4a2cc45085ad864b6785af2d7e2a4
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2024-05-09 19:24:48 +0700

    feat: change footer color

--- pages/petugas.php
+++ pages/petugas.php
@@ -175,7 +175,7 @@ $petugas = "SELECT * FROM petugas";
 <footer class=" nav-bottom" onclick="showFormWithTable()" onmousewheel="showAddDataForm()">
     <div id="btn-add">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+            <g  stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
                 <path d="M39 6H9a3 3 0 0 0-3 3v30a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3" />
                 <path d="m21 31l5 4l8-10M14 15h20m-20 8h8" />
             </g>
